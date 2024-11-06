8be3a92010c5a94e6f757c1107da95738478f6aa
NiklausRupail <m.rup.business@gmail.com>
2024-11-06 17:18:38 +0100

    Added counter example component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import Counter from './components/Counter/Counter'
 function App() {
-	return (
-		<>
-			<Nav />
-			<Header />
-			<Main />
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            <Nav />
+            <Header />
+            <Main />
+            <Counter />
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import styles from './Counter.module.scss'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <div className={styles.counter}>
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter
